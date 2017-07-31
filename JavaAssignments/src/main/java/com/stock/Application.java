72757241b787dafe5bab985b0ae59534d82d14c4
Ramya P <pramya0801@gmail.com>
2017-07-31 21:00:54 +0530

    28th

---
+++ JavaAssignments/src/main/java/com/stock/Application.java
@@ -0,0 +1,34 @@
+package com.stock;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.text.ParseException;
+import java.util.List;
+
+
+/**
+ * Created by SamayuSoftcorp on 28-07-2017.
+ */
+@SpringBootApplication
+
+public class Application implements CommandLineRunner {
+    @Autowired
+    StockFileProcessor processor ;
+       StockReader read;
+
+
+    public void run (String...strings) throws ParseException {
+        read.parseCSVFileToObject();
+               processor.process();
+
+
+        System.out.println("Welcome");
+
+    }
+    public static void main(String args[]){
+        org.springframework.boot.SpringApplication.run(Application.class,args);
+    }
+}
