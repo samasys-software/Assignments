84d5e66e35f61e444d23489411ef252882ef33f9
Ramya P <pramya0801@gmail.com>
2017-08-25 12:47:42 +0530

    StockQuotes

---
+++ StockManage/app/src/main/java/bussinessobjects/ResultInformation.java
@@ -0,0 +1,12 @@
+package bussinessobjects;
+
+import org.json.JSONObject;
+
+/**
+ * Created by SamayuSoftcorp on 10-08-2017.
+ */
+
+public class ResultInformation {
+    private JSONObject stockquotes;
+    private static final ResultInformation ourInstance = new ResultInformation();
+}
